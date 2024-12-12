0cba34c06ddc50a886bce7a3d5756a6cedbfdc81
foysalahamedsifat <foysalahamedsifat@gmail.com>
2024-12-12 11:28:01 -0500

    updated

--- BudgetBackend/JWTAuthentication/obj/Release/net8.0/JWTAuthentication.AssemblyInfo.cs
+++ BudgetBackend/JWTAuthentication/obj/Release/net8.0/JWTAuthentication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JWTAuthentication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+143708a7198cbda2ded72fedb50c6aeb1bd37d7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a937cb78302d0350ba90154c9ad6c2f347fd3dc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("JWTAuthentication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JWTAuthentication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
